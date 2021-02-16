29d7cc6570a79e67160272e0178b91bebe65b22b
Igor Vazhenin <65923504+Vazhenston@users.noreply.github.com>
2021-02-16 15:43:11 +0100

    Update environment.dart

--- application/sms_app/lib/environment.dart
+++ application/sms_app/lib/environment.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
 class Environment {
-  final apiUrl = 'http://192.168.0.45:8080/sms_sender/api/messages';
-  // final apiUrl = 'http://10.0.1.1:8080/sms_sender/api/messages';
+  final apiUrl = 'http://yourIp:port/sms_sender/api/messages';
+  // final apiUrl = 'http://yourIp:8080/sms_sender/api/messages';
 }
